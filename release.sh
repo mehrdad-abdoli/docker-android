451dc3120170c4179c4cd8c996da99686fe3ec83
Mehrdad Abdoli <mehrdad.abdoli@gmail.com>
2020-02-02 15:25:41 +0330

    proxy

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Bash version should >= 4 to be able to run this script.
+export https_proxy=http://devopt.net:8888
+export http_proxy=http://devopt.net:8888
 IMAGE="${DOCKER_ORG:-budtmo}/docker-android"
 
 if [ -z "$1" ]; then
