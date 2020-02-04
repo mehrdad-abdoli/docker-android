e24db5dc0922d2f9c4869dc5447829f594c5977a
mehrdad-abdoli <mehrdad.abdoli@gmail.com>
2020-02-04 20:27:51 +0330

    -append panic=1

--- src/app.py
+++ src/app.py
@@ -220,10 +220,10 @@ def run():
 
     if is_first_run:
         logger.info('Emulator was not previously initialized. Preparing a new one...')
-        cmd = 'emulator/emulator @{name} -gpu swiftshader_indirect -no-audio -verbose -wipe-data -writable-system -skip-adb-auth -qemu -append panic=1 {custom_args}'.format(name=avd_name, custom_args=custom_args)
+        cmd = 'emulator/emulator @{name} -gpu swiftshader_indirect -accel on -no-audio -verbose -wipe-data -writable-system -skip-adb-auth {custom_args}'.format(name=avd_name, custom_args=custom_args)
     else:
         logger.info('Using previously initialized AVD...')
-        cmd = 'emulator/emulator @{name} -gpu swiftshader_indirect -no-audio -verbose -writable-system -skip-adb-auth -qemu -append panic=1 {custom_args}'.format(name=avd_name, custom_args=custom_args)
+        cmd = 'emulator/emulator @{name} -gpu swiftshader_indirect -accel on -no-audio -verbose -writable-system -skip-adb-auth {custom_args}'.format(name=avd_name, custom_args=custom_args)
 
     appium = convert_str_to_bool(str(os.getenv('APPIUM', False)))
     if appium:
