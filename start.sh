90c1a966016364a6f129b0aa19b0fc39c0e4bc0b
mehrdad-abdoli <mehrdad.abdoli@gmail.com>
2020-02-05 13:46:25 +0330

    $app

--- start.sh
+++ start.sh
@@ -4,16 +4,18 @@ mob=$2
 function wait_emulator_to_be_ready () {
   boot_completed=false
   while [ "$boot_completed" == false ]; do
-    status=$(docker exec -i docker-android_$1 adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
+    status=$(docker exec -i docker-android_$1 adb wait-for-device shell getprop dev.bootcomplete | grep "1")
     echo "Boot Status: $status"
 
     if [ "$status" == "1" ]; then
       boot_completed=true
     else
-      sleep 1
+      sleep 5
     fi
   done
 }
+
+
 
 for i in $(seq 1 $app);
 do
@@ -24,7 +26,7 @@ done
 
 for i in $(seq 1 $mob);
 do
-  COMPOSE_HTTP_TIMEOUT=200 docker-compose up --scale AndroidApp=8 --scale MobileSite=$i --remove-orphans  -d
+  COMPOSE_HTTP_TIMEOUT=200 docker-compose up --scale AndroidApp=$app --scale MobileSite=$i --remove-orphans  -d
   echo "Start $i Emulator"
   wait_emulator_to_be_ready MobileSite_$i
 done
